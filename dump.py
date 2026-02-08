c5eb89eb9acf8d39adf87e072e40b848db2272d4
Ned Batchelder <ned@nedbatchelder.com>
2026-02-08 06:18:24 -0500

    show the ast

---
+++ dump.py
@@ -0,0 +1,30 @@
+from linklint import *
+
+
+def dump_doctree(node: nodes.Node, indent: int = 0) -> None:
+    """Print a nicely formatted tree of a docutils doctree."""
+    prefix = "  " * indent
+    if isinstance(node, nodes.Text):
+        text = node.astext()
+        # if len(text) > 60:
+        #     text = text[:57] + "..."
+        print(f"{prefix}Text: {text!r}")
+    else:
+        tag = node.__class__.__name__
+        attrs = {}
+        for key in ("ids", "names", "reftype", "reftarget", "refuri"):
+            val = node.get(key)
+            if val:
+                attrs[key] = val
+        attr_str = f" {attrs}" if attrs else ""
+        line = node.get("line") or getattr(node, "line", None)
+        line_str = f" L{line}" if line else ""
+        print(f"{prefix}{tag}{attr_str}{line_str}")
+        for child in node.children:
+            dump_doctree(child, indent + 1)
+
+
+with open("weakref.rst") as f:
+    tree = parse_rst_file(f.read())
+
+dump_doctree(tree)
