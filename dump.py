71693eaef00fa3c7e59f78eb6fdd8fb91a348687
Ned Batchelder <ned@nedbatchelder.com>
2026-02-18 07:41:18 -0500

    more information in the doctree dump

--- dump.py
+++ dump.py
@@ -1,6 +1,21 @@
 import sys
 
-from linklint import *
+from docutils import nodes
+from linklint import parse_rst_file
+
+INTERESTING_ATTRS = [
+    "ids",
+    "names",
+    "reftype",
+    "reftarget",
+    "refuri",
+    "domain",
+    "objtype",
+    "desctype",
+    "class",
+    "fullname",
+    "module",
+]
 
 
 def dump_doctree(node: nodes.Node, indent: int = 0) -> None:
@@ -13,15 +28,15 @@ def dump_doctree(node: nodes.Node, indent: int = 0) -> None:
         print(f"{prefix}Text: {text!r}")
     else:
         tag = node.__class__.__name__
-        attrs = {}
-        for key in ("ids", "names", "reftype", "reftarget", "refuri"):
-            val = node.get(key)
-            if val:
-                attrs[key] = val
-        attr_str = f" {attrs}" if attrs else ""
+        attrs = []
+        for key in INTERESTING_ATTRS:
+            if val := node.get(key):
+                attrs.append(f"{key}={val!r}")
+        attr_str = f" {{{', '.join(attrs)}}}" if attrs else ""
         line = node.get("line") or getattr(node, "line", None)
         line_str = f" L{line}" if line else ""
         print(f"{prefix}{tag}{attr_str}{line_str}")
+        #print(f"{prefix}{node.attributes}")
         for child in node.children:
             dump_doctree(child, indent + 1)
 
