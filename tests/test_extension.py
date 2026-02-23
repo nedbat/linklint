e62da9c9780677c12e3ef29a2c63238af1a0bd50
Ned Batchelder <ned@nedbatchelder.com>
2026-02-23 08:06:11 -0500

    all tests/data/*.rst get summarized

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -1,7 +1,13 @@
-import os
+"""
+Tests using .rst files in tests/data.
+
+Sphinx renders them to HTML, then we compare a summary of the HTML to an
+expected summary in a corresponding .html file.
+
+"""
+
 import shutil
 from pathlib import Path
-from typing import Iterable
 
 import pytest
 
@@ -13,19 +19,14 @@ from summarize_html import summarize_html_file
 
 PROJECT = Path(__file__).parent.parent
 
-def summarizable_rst_files() -> Iterable[str]:
-    data_files = set(os.listdir("tests/data"))
-    for df in data_files:
-        if df.endswith(".rst"):
-            root = df.removesuffix(".rst")
-            summary = root + "_summary.html"
-            if summary in data_files:
-                yield root
 
-
-@pytest.mark.parametrize("root", summarizable_rst_files())
-def test_summarize_html(root: str) -> None:
-    rst = (PROJECT / "tests/data" / f"{root}.rst").read_text()
+@pytest.mark.parametrize(
+    "rst_file",
+    [r.name for r in Path("tests/data").glob("*.rst")],
+)
+def test_summarize_html(rst_file: str) -> None:
+    rst = (PROJECT / "tests/data" / rst_file).read_text()
+    root = rst_file.removesuffix(".rst")
     with in_tempdir():
         run_sphinx(rst, buildername="html", extensions=["linklint.ext"])
         summary = summarize_html_file("_build/index.html")
@@ -34,7 +35,11 @@ def test_summarize_html(root: str) -> None:
         shutil.copyfile("_build/index.html", PROJECT / "tmp/html" / f"{root}.html")
 
     (PROJECT / "tmp/html" / f"{root}_summary.html").write_text(summary)
-    expected = (PROJECT / "tests/data" / f"{root}_summary.html").read_text()
+    summary_file = PROJECT / "tests/data" / f"{root}_summary.html"
+    if summary_file.exists():
+        expected = summary_file.read_text()
+    else:
+        expected = f"Summary {summary_file} doesn't exist"
 
     if summary != expected:
         print(f"Full HTML is at tmp/html/{root}.html")
