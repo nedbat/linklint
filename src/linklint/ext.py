e14aadc3718484656e71e1af782c598892e2de32
Ned Batchelder <ned@nedbatchelder.com>
2026-02-24 07:43:26 -0500

    sphinx env data is copied around, accumulate it properly

--- src/linklint/ext.py
+++ src/linklint/ext.py
@@ -13,17 +13,19 @@ logger = logging.getLogger(__name__)
 
 
 def init_data(app: Sphinx) -> None:
-    app.env.linklint_count: int = 0
+    app.env.linklint_counts: dict[str, int] = {}
 
 
 def process_reference_nodes(app, doctree):
     """Find references that shouldn't be links, and un-reference them."""
     # Change <reference><literal></reference> to just <literal>.
 
+    count = 0
     for finder in (find_self_refs, find_duplicate_refs):
         for ref in finder(doctree):
             cast(nodes.Element, ref.parent).replace(ref, ref.children[0])
-            app.env.linklint_count += 1
+            count += 1
+    app.env.linklint_counts[app.env.docname] = count
 
 
 def merge_data(
@@ -32,13 +34,17 @@ def merge_data(
     docnames: list[str],
     other: BuildEnvironment,
 ) -> None:
-    env.linklint_count += other.linklint_count  # type: ignore
+    # Env's start as copies of other envs, so we have to be careful to only
+    # merge data for the docnames that are actually being merged.
+    for docname in docnames:
+        if docname in other.linklint_counts:  # type: ignore
+            env.linklint_counts[docname] = other.linklint_counts[docname]  # type: ignore
 
 
 def display_results(app: Sphinx, exception: Exception | None) -> None:
     if not exception:
-        assert hasattr(app.env, "linklint_count")
-        logger.info(f"Linklint unlinked {app.env.linklint_count} references.")
+        total = sum(app.env.linklint_counts.values())  # type: ignore
+        logger.info(f"Linklint unlinked {total} references.")
 
 
 def setup(app: Sphinx) -> ExtensionMetadata:
